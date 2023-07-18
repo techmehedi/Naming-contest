6cdcc8f9d188c9f27b74c10853b6fa7c38c63518
techmehedi <ecommehedi@gmail.com>
2023-07-17 23:56:11 -0400

    initial

---
+++ src/server/config.ts
@@ -0,0 +1,11 @@
+const env = process.env
+
+export const PORT = env.PORT ?? "8080";
+export const HOST = env.host ?? "0.0.0.0"
+export const SERVER_URL = `http://${HOST}:${PORT}`
+
+export default {
+    PORT,
+    HOST,
+    SERVER_URL,
+}
---
+++ src/server/server.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import config, { PORT } from "./config";
+import os from "node:os";
+
+const server = express();
+
+server.use(express.static("dist"));
+
+server.set("view engine", "ejs");
+
+server.use("/", (req, res) => {
+  res.render("index", {
+    content: "EJS is cool",
+  });
+});
+
+server.listen(config.PORT,config.HOST, () => {
+  console.info(
+    `Express Server is listening at ${config.SERVER_URL}`,
+    `Free Mem: ${os.freemem() / 1024 / 1024}`
+  );
+});
+
